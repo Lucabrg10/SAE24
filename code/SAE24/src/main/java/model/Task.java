1c877e96152ac91fc84468b473434b88d162ac6b
Luca10 <brugnettiluca@gmail.com>
2024-12-05 14:25:16 +0100

    Update Task.java
    aggiunta orario inizio, fine e data

--- code/SAE24/src/main/java/model/Task.java
+++ code/SAE24/src/main/java/model/Task.java
@@ -1,5 +1,7 @@
 package model;
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 public class Task {
@@ -7,23 +9,34 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
     @JoinColumn(name = "commessa_id")
     private Commessa commessa;
+
     @ManyToOne
     @JoinColumn(name = "dipendente_id")
     private Dipendente dipendente;
+
     private String stato; 
-    private Double tempoEffettivo;
+    private Double tempoEffettivo; 
+
+    private LocalDateTime orarioInizio; 
+    private LocalDateTime orarioFine;   
+    private LocalDate data;             
+
     public Task() {}
 
-    public Task(Commessa commessa, Dipendente dipendente, String stato, Double tempoEffettivo) {
+    public Task(Commessa commessa, Dipendente dipendente, String stato, LocalDateTime orarioInizio, LocalDateTime orarioFine, LocalDate data) {
         this.commessa = commessa;
         this.dipendente = dipendente;
         this.stato = stato;
-        this.tempoEffettivo = tempoEffettivo;
+        this.orarioInizio = orarioInizio;
+        this.orarioFine = orarioFine;
+        this.data = data;
     }
 
+    // Getter e Setter
     public Long getId() {
         return id;
     }
@@ -64,14 +77,43 @@ public class Task {
         this.tempoEffettivo = tempoEffettivo;
     }
 
+    public LocalDateTime getOrarioInizio() {
+        return orarioInizio;
+    }
+
+    public void setOrarioInizio(LocalDateTime orarioInizio) {
+        this.orarioInizio = orarioInizio;
+    }
+
+    public LocalDateTime getOrarioFine() {
+        return orarioFine;
+    }
+
+    public void setOrarioFine(LocalDateTime orarioFine) {
+        this.orarioFine = orarioFine;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
     @Override
     public String toString() {
         return "Task{" +
                 "id=" + id +
-                ", commessa=" + commessa.getId() +
-                ", dipendente=" + dipendente.getId() +
+                ", commessa=" + (commessa != null ? commessa.getId() : null) +
+                ", dipendente=" + (dipendente != null ? dipendente.getId() : null) +
                 ", stato='" + stato + '\'' +
                 ", tempoEffettivo=" + tempoEffettivo +
+                ", orarioInizio=" + orarioInizio +
+                ", orarioFine=" + orarioFine +
+                ", data=" + data +
                 '}';
     }
 }
+
+       
