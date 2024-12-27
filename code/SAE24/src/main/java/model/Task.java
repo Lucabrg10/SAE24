d59bd256469ec57a59b2559d9a078074dc2d71f1
Luca10 <brugnettiluca@gmail.com>
2024-12-27 15:17:35 +0100

    Update Task.java

--- code/SAE24/src/main/java/model/Task.java
+++ code/SAE24/src/main/java/model/Task.java
@@ -20,7 +20,6 @@ public class Task {
 
     private String stato; 
     private Double tempoEffettivo; 
-
     private LocalDateTime orarioInizio; 
     private LocalDateTime orarioFine;   
     private LocalDate data;             
@@ -35,72 +34,54 @@ public class Task {
         this.orarioFine = orarioFine;
         this.data = data;
     }
-
-    // Getter e Setter
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public Commessa getCommessa() {
         return commessa;
     }
-
     public void setCommessa(Commessa commessa) {
         this.commessa = commessa;
     }
-
     public Dipendente getDipendente() {
         return dipendente;
     }
-
     public void setDipendente(Dipendente dipendente) {
         this.dipendente = dipendente;
     }
-
     public String getStato() {
         return stato;
     }
-
     public void setStato(String stato) {
         this.stato = stato;
     }
-
     public Double getTempoEffettivo() {
         return tempoEffettivo;
     }
-
     public void setTempoEffettivo(Double tempoEffettivo) {
         this.tempoEffettivo = tempoEffettivo;
     }
-
     public LocalDateTime getOrarioInizio() {
         return orarioInizio;
     }
-
     public void setOrarioInizio(LocalDateTime orarioInizio) {
         this.orarioInizio = orarioInizio;
     }
-
     public LocalDateTime getOrarioFine() {
         return orarioFine;
     }
-
     public void setOrarioFine(LocalDateTime orarioFine) {
         this.orarioFine = orarioFine;
     }
-
     public LocalDate getData() {
         return data;
     }
-
     public void setData(LocalDate data) {
         this.data = data;
     }
-
     @Override
     public String toString() {
         return "Task{" +
